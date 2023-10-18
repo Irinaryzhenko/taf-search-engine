817ed52254a9067f72755f14bf6359884d6a9e2e
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-10-18 16:02:23 +0300

    reformat code

--- src/test/java/testRunner/RunTest.java
+++ src/test/java/testRunner/RunTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = "pretty",
-        tags = "@smoke",
         features = "src/test/resources/features",
         glue = "java/steps/api",
         monochrome = true)
